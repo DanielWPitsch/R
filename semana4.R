74da5b963f177668bfb9724986d4274ebcbb6d54
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2022-12-09 22:54:01 -0300

    up3

--- semana4.R
+++ semana4.R
@@ -37,3 +37,38 @@ ggplot(data = penguins) +geom_point(mapping = aes(x = flipper_length_mm, y = bod
 ggplot(data = penguins) +geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species, shape=species, size=species))
 
 ggplot(data = penguins) +geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, alpha=species), color="purple")
+
+ggplot(data = penguins) +geom_line(mapping = aes(x = flipper_length_mm, y = body_mass_g, alpha=species), color="purple")
+
+ggplot(data = penguins) +geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g, alpha=species)) +geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species))
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut, fill=cut))
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut, fill=clarity))
+
+ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g))+
+  geom_point(mapping = aes(color=species))+
+  facet_wrap(~species)
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=color, fill=cut))+
+  facet_wrap(~cut)
+
+#ggplot(data = penguins))+
+#  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species))+
+#  facet_grid(~sex)
+
+ggplot(data = penguins) +geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))+
+  labs(title="Palme pingins: massa corporal vs tamanho da nadadeira", 
+       subtitle="subtitulo",
+       caption="dados coletados pela Dra. Kristen Gorman")+
+  annotate("text", x=220, y=3500, label="The Gentos are the largest",
+           color="blue", fontface="bold", size=4.5, angle=10)
+
+ggsave("teste13.png")
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = color, fill = cut)) +
+  facet_wrap(~cut)
